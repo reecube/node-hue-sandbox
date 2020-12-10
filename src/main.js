39f0bec5fa174284734a509a2960b3b127435be3
Yves Riedener <git@reecube.com>
2020-12-10 20:44:35 +0100

    bridge auto discovery implemented

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 const huejay = require('huejay');
 
-const emptyOptions = undefined;
-
 const errorHandler = error => {
     console.error(`An error occurred: ${error.message}`);
 };
 
-huejay.discover(emptyOptions)
-    .then(bridges => {
-        for (let bridge of bridges) {
-            console.log(bridge);
-        }
-    })
-    .catch(errorHandler);
+const discover = require('./middleware/discover');
+
+(async () => {
+    const bridge = await discover(huejay, errorHandler);
+
+    // TODO continue here
+    console.log(`TODO`, bridge)
+})();
+
+
---
+++ src/middleware/discover.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+const isReachable = require('is-reachable');
+
+const bridgeStoragePath = './var/bridge.json';
+
+module.exports = (huejay) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (fs.existsSync(bridgeStoragePath)) {
+                const data = fs.readFileSync(bridgeStoragePath, 'utf8');
+                const bridge = JSON.parse(data);
+
+                if (!await isReachable(bridge.ip)) {
+                    reject(new Error('Selected bridge unreachable, check the file!'));
+
+                    return;
+                }
+
+                resolve(bridge);
+
+                return;
+            }
+
+            /**
+             * @type {Array}
+             */
+            const bridges = await huejay.discover(undefined);
+
+            if (bridges.length === 1) {
+                const bridge = bridges[0];
+
+                try {
+                    fs.writeFileSync(bridgeStoragePath, JSON.stringify(bridge))
+                } catch (error) {
+                    reject(error);
+                }
+
+                resolve(bridge);
+
+                return;
+            }
+
+            console.log(`${bridges.length} bridges found. Please select one of them manually by adding the file:`)
+            for (let bridge of bridges) {
+                console.log(`- ${bridge}`);
+            }
+
+            reject(new Error('Too many bridges found!'));
+        } catch (error) {
+            reject(error);
+        }
+    });
+
+}
